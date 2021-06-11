56e79cdeb4fbaadaea1e20e38d3cbff18295fffb
Harshit Kumar <kumarharshit850@gmail.com>
2021-06-11 20:14:47 +0530

    Front end completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
 
 const App = () => {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomePage} exact />
+          <Route path='/product/:id' component={ProductPage} exact />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 
