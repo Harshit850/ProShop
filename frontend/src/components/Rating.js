3e8c7da687362a6ab6f6c9d8e7a45cdbcdc2599a
Harshit Kumar <kumarharshit850@gmail.com>
2021-06-13 19:07:45 +0530

    products served from backend

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -3,66 +3,68 @@ import PropTypes from 'prop-types';
 
 const Rating = ({ value, text, color }) => {
   return (
-    <div className='rating'>
+    <div className='rating d-flex justify-content-around'>
       <span>
-        <i
-          style={{ color: color }}
-          className={
-            value >= 1.0
-              ? 'fas fa-star'
-              : value >= 0.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color: color }}
-          className={
-            value >= 2.0
-              ? 'fas fa-star'
-              : value >= 1.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color: color }}
-          className={
-            value >= 3.0
-              ? 'fas fa-star'
-              : value >= 2.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color: color }}
-          className={
-            value >= 4.0
-              ? 'fas fa-star'
-              : value >= 3.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          style={{ color: color }}
-          className={
-            value >= 5.0
-              ? 'fas fa-star'
-              : value >= 4.5
-              ? 'fas fa-star-half-alt'
-              : 'far fa-star'
-          }
-        ></i>
+        <span>
+          <i
+            style={{ color: color }}
+            className={
+              value >= 1.0
+                ? 'fas fa-star'
+                : value >= 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            style={{ color: color }}
+            className={
+              value >= 2.0
+                ? 'fas fa-star'
+                : value >= 1.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            style={{ color: color }}
+            className={
+              value >= 3.0
+                ? 'fas fa-star'
+                : value >= 2.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            style={{ color: color }}
+            className={
+              value >= 4.0
+                ? 'fas fa-star'
+                : value >= 3.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+        <span>
+          <i
+            style={{ color: color }}
+            className={
+              value >= 5.0
+                ? 'fas fa-star'
+                : value >= 4.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
       </span>
       <span>{text && text}</span>
     </div>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import axios from 'axios';
 
 import Product from '../components/Product';
-import products from '../products';
 
 const HomePage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4}>
-            <Product product={product} color='red' />
+            <Product product={product} />
           </Col>
         ))}
       </Row>
